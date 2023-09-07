756a5e318518378be5d883a7d60375252adaa4ff
nyeka <nyeka2101@gmail.com>
2023-09-07 10:48:26 +0700

    feat: adding vercel analytics

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
 import "./tailwind.css";
+import { inject } from "@vercel/analytics";
 
 import App from "./App.vue";
 import router from "./router";
 
 const app = createApp(App);
 
+inject();
 app.use(createPinia());
 app.use(router);
 
